11ac5f216fd1d73ba4174efc5e9f3b34a62d0b86
Artutu64 <arthur.rimaudiere@free.fr>
2024-04-11 17:49:24 +0000

    patchs + ajouts values

--- network/network_statistics.py
+++ network/network_statistics.py
@@ -16,8 +16,8 @@ def main():
     
     print("Loaded config for DDOS detection: " + str(config) + " \n")
 
-    NUMBER_OF_PACKETS = 0
     while True:
+        NUMBER_OF_PACKETS = 0
         t.sleep(15)
         hashMap = {}
         traffic = loadData()
@@ -37,10 +37,10 @@ def main():
             if (key in data) == False:
                 data[key] = []
             data[key].append((hashMap[key][0], hashMap[key][1]))
-        
         for key in data:
             if (key in hashMap) == False:
-                data[key].append((0,0))
+                hashMap[key] = []
+                hashMap[key].append((0,0))
             
             start_index = len(hashMap[key])-16
             if start_index < 0:
@@ -93,34 +93,45 @@ def start_analysis(data, NUMBER_OF_PACKETS, config):
 
 def analysis(port, values, NUMBER_OF_PACKETS, config, percentPacketsAllowed):
 
-    print("ANALYSE DU PORT " + str(port) + " EN COURS !!!")
+    print("----------------------------------\nStarting analyse of port: " + str(port))
 
     data = [values[len(values)-1-i] for i in range(len(values))]
 
     precedent_values = []
     for i in range(len(data)):
         amount, size = data[i]
-        precedent_values.append(values)
+        precedent_values.append(amount)
 
-    if len(data) > 0:
+    if len(precedent_values) <= 1:
+        print("Impossible to do analysis because there is only one value !")
+
+    if len(data) > 0 and len(precedent_values) > 1:
+        print("Values: " + str(len(data)))
         l_a, l_s = data[0]
         savePortValue(port, l_a, l_s)
 
         detection_number = config['request_before_warnings']
+        print("Total number of packets: " + str(NUMBER_OF_PACKETS))
+        print("Detection number: " + str(detection_number))
+        print("Number of packets: " + str(l_a))
+        print("Average size of packets: " + str(l_s))
 
         if NUMBER_OF_PACKETS > detection_number:
 
             percent = l_a / NUMBER_OF_PACKETS
 
+            print("Calculated %: " + str(percent*100))
+            print("Max allowed: " + str(percentPacketsAllowed*100.0))
+
             # We're checking if it is greater than the threshold
-            if percent > percentPacketsAllowed:
+            if percent >= percentPacketsAllowed:
 
                 # Let's load data from the file
                 n = 0
                 size_of_packets = 0
 
                 try:
-                    with open('web/data/ports/history' + str(port) + '.csv', 'r') as files:
+                    with open('web/data/ports/history_' + str(port) + '.csv', 'r') as files:
                         for line in files:
                             splitted = line.split()
                             if len(splitted) == 2:
@@ -145,6 +156,10 @@ def analysis(port, values, NUMBER_OF_PACKETS, config, percentPacketsAllowed):
                 less_value = size_of_packets - step
                 great_value = size_of_packets + step
 
+                print("Less value: " + str(less_value))
+                print("Max value: " + str(great_value))
+                print("Value calculted: " + str(l_s))
+
                 if l_s < less_value:
                     create_log_file("WARNING", "PORT " + str(port), "The size of packet received on port is anormally small.")
                 elif l_s > great_value:
@@ -156,8 +171,10 @@ def analysis(port, values, NUMBER_OF_PACKETS, config, percentPacketsAllowed):
 
                 # Check if the value is abnormal
                 if is_outlier(precedent_values, l_a):
+                    print("\nWarning: Abnormal value detected !")
                     # checking if error is near predicted value with 20% of error
-                    if is_predictable(precedent_values, 0.2) == False:
+                    if is_predictable(precedent_values, l_a, 0.2) == False:
+                        print("\nValue was not predictable.\n")
                         create_log_file("WARNING", "PORT " + str(port), "Receiving unexpected huge amount of request on port " + str(port))
 
         
@@ -167,6 +184,8 @@ def analysis(port, values, NUMBER_OF_PACKETS, config, percentPacketsAllowed):
 def is_predictable(data, recent_value, max_relative_error):
     # Calculate the linear trend based on previous data points
     n = len(data)
+    print("")
+    print("     Length of data:", len(data))
     
     if n < 2:
         return False  # Need at least two data points to estimate a trend
@@ -176,16 +195,16 @@ def is_predictable(data, recent_value, max_relative_error):
     
     # Calculate the mean difference (approximating the linear trend)
     mean_difference = sum(differences) / (n - 1)
+    print("     Mean difference: ", mean_difference)
     
     # Predict the next value by extrapolating the linear trend
     predicted_next_value = data[-1] + mean_difference
-    
+    print("     Predicted next value: ", predicted_next_value)
     # Calculate the absolute and relative errors between the recent value and the predicted next value
     absolute_error = abs(recent_value - predicted_next_value)
     relative_error = absolute_error / predicted_next_value
-
-    # Print the relative error and absolute error for debugging purposes
-    print(f"Relative Error: {relative_error}, Absolute Error: {absolute_error}")
+    print("     Absolute error:", absolute_error)
+    print("     Relative error:", relative_error)
     
     # Check if the relative error is within the specified threshold to consider the recent value predictable
     if relative_error <= max_relative_error:
@@ -195,12 +214,22 @@ def is_predictable(data, recent_value, max_relative_error):
 
 def is_outlier(data, value):
     # Calculate the mean and standard deviation of the data
+    print("")
+
+    print("     Length of data:", len(data))
     mean_value = statistics.mean(data)
+    print("     Mean value:", mean_value)
     std_dev = statistics.stdev(data)
+    print("     Standart deviation:", std_dev)
     
     # Calculate the z-score for the given value
-    z_score = (value - mean_value) / std_dev
+    z_score = 9999999999
+    if std_dev > 0:
+        z_score = (value - mean_value) / std_dev
+    elif std_dev == 0:
+        z_score = 0
     
+    print("     Z-score:", z_score)
     # Define a threshold for determining if the value is an outlier
     outlier_threshold = 2  # Typical threshold for z-score
     
